30ccc7399658ad7d874e5043a1f06d906fdcdffe
He Qingbao <qingbao.ho@gmail.com>
2018-05-05 23:54:14 +0800

    Authorised routes

---
+++ src/components/routes/UserRoute.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Redirect, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+const UserRoute = ({isAuthenticated, component: Component, ...rest}) => (
+    <Route {...rest} render={
+        props => isAuthenticated ? <Component {...props}/> : <Redirect to="/"/>
+    }/>
+);
+
+UserRoute.propTypes = {
+    component: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.user.token
+    }
+}
+
+export default connect(mapStateToProps)(UserRoute);
