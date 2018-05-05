2b020313ed99fda46871c0ddcf131e2208d76d7d
He Qingbao <qingbao.ho@gmail.com>
2018-05-05 19:50:23 +0800

    Login/Logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,13 @@
-import {USER_LOGGED_IN} from '../types';
+import {USER_LOGGED_IN, USER_LOGGED_OUT} from '../types';
 import api from '../api';
 
 export const userLoggedIn = user => ({
     type: USER_LOGGED_IN,
     user
+});
+
+export const userLoggedOut = () => ({
+    type: USER_LOGGED_OUT
 });
 
 // export const login = credentials => dispatch =>
@@ -12,7 +16,17 @@ export const userLoggedIn = user => ({
 export function login(credentials) {
 
     return function tmp(dispatch) {
-        return api.user.login(credentials).then(user => dispatch(userLoggedIn(user)));
+        return api.user.login(credentials).then(user => {
+            localStorage.bookwormJWT = user.token;
+            dispatch(userLoggedIn(user))
+        });
     }
 
+}
+
+export function logout() {
+    return function tmp(dispatch) {
+        localStorage.removeItem('bookwormJWT');
+        dispatch(userLoggedOut());
+    }
 }
