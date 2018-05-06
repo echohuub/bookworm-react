2fd9761d35313ab5fd2fee1a936044407156aebf
He Qingbao <qingbao.ho@gmail.com>
2018-05-06 18:46:02 +0800

    Search book mock

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 import DashboardPage from './components/pages/DashboardPage';
 import SignupPage from './components/pages/SignupPage';
 import ResetPasswordPage from './components/pages/ResetPasswordPage';
+import NewBookPage from './components/pages/NewBookPage';
 import ComfirmationPage from './components/pages/ComfirmationPage';
 import ForgotPasswordPage from './components/pages/ForgotPasswordPage';
 import UserRoute from './components/routes/UserRoute';
 import GuestRoute from './components/routes/GuestRoute';
+import TopNavigation from './components/navigation/TopNavigation';
 
-const App = ({location}) => (
+const App = ({location, isAuthenticated}) => (
     <div className="ui container">
+        {isAuthenticated && <TopNavigation/>}
+
         <Route location={location} path="/" exact component={HomePage}/>
         <Route
             location={location}
@@ -35,14 +40,30 @@ const App = ({location}) => (
             path="/forgot_password"
             exact
             component={ForgotPasswordPage}/>
-        <UserRoute location={location} path="/dashboard" exact component={DashboardPage}/>
+        <UserRoute
+            location={location}
+            path="/dashboard"
+            exact
+            component={DashboardPage}/>
+        <UserRoute
+            location={location}
+            path="/books/new"
+            exact
+            component={NewBookPage}/>
     </div>
 );
 
 App.propTypes = {
     location: PropTypes.shape({
         pathname: PropTypes.string.isRequired
-    }).isRequired
+    }).isRequired,
+    isAuthenticated: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.user.email
+    }
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
---
+++ src/components/navigation/TopNavigation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Dropdown, Image, Menu} from 'semantic-ui-react';
+import gravatarUrl from 'gravatar-url';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import * as actions from '../../actions/auth';
+
+const TopNavigation = ({user, logout}) => (
+    <Menu secondary pointing>
+        <Menu.Item as={Link} to="/dashboard">
+            Dashboard
+        </Menu.Item>
+
+        <Menu.Menu position="right">
+            {console.log(user.email)}
+            <Dropdown trigger={<Image avatar src={gravatarUrl(user.email)}/>}>
+                <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => logout()}>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </Menu.Menu>
+    </Menu>
+);
+
+TopNavigation.propTypes = {
+    user: PropTypes.shape({
+        email: PropTypes.string.isRequired
+    }).isRequired,
+    logout: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, {logout: actions.logout})(TopNavigation);
